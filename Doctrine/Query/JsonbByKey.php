8b15a7548581bdf861541d9e62be532686d3d88d
Fabiano <fabiano.fa04@gmail.com>
2018-01-06 23:09:59 -0200

    Fix comments

--- Doctrine/Query/JsonbByKey.php
+++ Doctrine/Query/JsonbByKey.php
@@ -4,6 +4,14 @@ namespace fabianofa\JsonbOperatorsBundle\Doctrine\Query;
 use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 
+/**
+ * jsonbbykeytext DQL function is the the equivalent SQL:
+ *
+ * jsonbcolumn->'property1'->'property2'
+ *
+ * For more information about jsonb operators, visit the official documentation:
+ * https://www.postgresql.org/docs/current/static/functions-json.html
+ */
 class JsonbByKey extends FunctionNode
 {
     public $column = null;
@@ -21,7 +29,7 @@ class JsonbByKey extends FunctionNode
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
-        // The rightHandSide argument is wrapped with single quotes. For
+        // The parth argument is wrapped with single quotes. For
         // a valid SQL syntax, these have to be removed.
         $path = str_replace("'", "", $this->path->dispatch($sqlWalker));
         $path = explode(",", $path);
--- Doctrine/Query/JsonbByPath.php
+++ Doctrine/Query/JsonbByPath.php
@@ -5,15 +5,9 @@ use Doctrine\ORM\Query\AST\Functions\FunctionNode;
 use Doctrine\ORM\Query\Lexer;
 
 /**
- * jsonbvl DQL function uses the notation #>>'{path}' to retrieve the value from
- * the path given the first parameters. This function would be translated as:
+ * jsonbbypath DQL function is the the equivalent SQL:
  *
- * (jsonbcolumn#>>'{property1,property2}')
- *
- * The syntax to use it as DQL, using the default configuration included in
- * this bundle is:
- *
- * jsonvl(jsonbcolumn, 'property1.property2')
+ * jsonbcolumn#>>'{property1,property2}'
  *
  * For more information about jsonb operators, visit the official documentation:
  * https://www.postgresql.org/docs/current/static/functions-json.html
@@ -35,7 +29,7 @@ class JsonbByPath extends FunctionNode
 
     public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker)
     {
-        // The rightHandSide argument is wrapped with single quotes. For
+        // The parth argument is wrapped with single quotes. For
         // a valid SQL syntax, these have to be removed.
         $path = str_replace("'", "", $this->path->dispatch($sqlWalker));
 
